7c2557a43def24bb924b18f3d2a50e64e463eef2
Kyle <kyle@zenput.com>
2020-08-27 22:58:24 -0600

    Add debug file delete button

---
+++ components/DeleteButton/index.tsx
@@ -0,0 +1,40 @@
+import React from "react";
+import { Text, View, StyleSheet, TouchableOpacity } from "react-native";
+import * as FileSystem from "expo-file-system";
+
+const styles = StyleSheet.create({
+  buttonText: {
+    fontSize: 24,
+  },
+  container: { flex: 1, padding: 16 },
+});
+
+const DeleteButton = () => (
+  <View style={styles.container}>
+    <TouchableOpacity
+      style={{
+        justifyContent: "center",
+        alignItems: "center",
+        backgroundColor: "#DDDDDD",
+        flex: 1,
+      }}
+      onPress={async () => {
+        const files = await FileSystem.readDirectoryAsync(
+          FileSystem.documentDirectory!
+        );
+
+        console.log(files);
+
+        files.forEach(async (file) => {
+          const uri = FileSystem.documentDirectory + file;
+          console.log(uri);
+          await FileSystem.deleteAsync(uri);
+        });
+      }}
+    >
+      <Text style={styles.buttonText}>Delete Files</Text>
+    </TouchableOpacity>
+  </View>
+);
+
+export default DeleteButton;
