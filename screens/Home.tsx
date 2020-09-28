7c935e46f0135a90711382d7c518fca4aca0eb3f
Mike north <north@cofactor.systems>
2020-09-28 11:17:28 -0700

    changed the name of file and order of buttons

--- screens/Home.tsx
+++ screens/Home.tsx
@@ -22,7 +22,7 @@ const styles = StyleSheet.create({
 const METAL = "metal.mp3";
 const INSTRUCTIONS = "instructions.mp3";
 const RIVER_MASTER = "river-master.mp3";
-const SEVEN_MINUTE_RESET = "7-min-reset.mp3";
+const BEACH_DAY = "beach-day.mp3";
 const RIDE_THE_TIGER = "ride-the-tiger.mp3";
 
 const playTrack = async (
@@ -106,16 +106,16 @@ const Home = () => {
         <Progress.Bar progress={progress} width={null} color={mebaGreen} />
       )}
       <View style={{ flex: 1, flexDirection: "row" }}>
+        <MusicButton track={BEACH_DAY} {...musicButtonProps}>
+          <Text style={styles.buttonText}>RELAX</Text>
+        </MusicButton>
         <MusicButton track={RIVER_MASTER} {...musicButtonProps}>
           <Text style={styles.buttonText}>DE-STRESS</Text>
         </MusicButton>
-        <MusicButton track={RIDE_THE_TIGER} {...musicButtonProps}>
-          <Text style={styles.buttonText}>FLOW</Text>
-        </MusicButton>
       </View>
       <View style={{ flex: 1, flexDirection: "row" }}>
-        <MusicButton track={SEVEN_MINUTE_RESET} {...musicButtonProps}>
-          <Text style={styles.buttonText}>RESET</Text>
+        <MusicButton track={RIDE_THE_TIGER} {...musicButtonProps}>
+          <Text style={styles.buttonText}>FLOW</Text>
         </MusicButton>
         {/* The DeleteButton below is meant to be use for debugging. 
         It will delete any local meba files that were previously downloaded to your device. */}
