9e0cf1a55a06790efdeae61379397c32217b6d45
Kyle Stewart <kgstew@gmail.com>
2020-12-03 20:05:37 -0800

    Add back tab navigator

--- navigation/BottomTabNavigator.tsx
+++ navigation/BottomTabNavigator.tsx
@@ -9,43 +9,43 @@ import Home from "../screens/Home";
 import TabTwoScreen from "../screens/TabTwoScreen";
 import { BottomTabParamList, TabOneParamList, TabTwoParamList } from "../types";
 
-// const BottomTab = createBottomTabNavigator<BottomTabParamList>();
+const BottomTab = createBottomTabNavigator<BottomTabParamList>();
 
-// export default function BottomTabNavigator() {
-//   const colorScheme = useColorScheme();
+export default function BottomTabNavigator() {
+  const colorScheme = useColorScheme();
 
-//   return (
-//     <BottomTab.Navigator
-//       initialRouteName="Home"
-//       tabBarOptions={{ activeTintColor: Colors[colorScheme].tint }}
-//     >
-//       <BottomTab.Screen
-//         name="Home"
-//         component={TabOneNavigator}
-//         options={{
-//           tabBarIcon: ({ color }) => (
-//             <TabBarIcon name="ios-code" color={color} />
-//           ),
-//         }}
-//       />
-//       {/* <BottomTab.Screen
-//         name="TabTwo"
-//         component={TabTwoNavigator}
-//         options={{
-//           tabBarIcon: ({ color }) => (
-//             <TabBarIcon name="ios-code" color={color} />
-//           ),
-//         }}
-//       /> */}
-//     </BottomTab.Navigator>
-//   );
-// }
+  return (
+    <BottomTab.Navigator
+      initialRouteName="Home"
+      tabBarOptions={{ activeTintColor: Colors[colorScheme].tint }}
+    >
+      <BottomTab.Screen
+        name="Home"
+        component={TabOneNavigator}
+        options={{
+          tabBarIcon: ({ color }) => (
+            <TabBarIcon name="ios-code" color={color} />
+          ),
+        }}
+      />
+      <BottomTab.Screen
+        name="TabTwo"
+        component={TabTwoNavigator}
+        options={{
+          tabBarIcon: ({ color }) => (
+            <TabBarIcon name="ios-code" color={color} />
+          ),
+        }}
+      />
+    </BottomTab.Navigator>
+  );
+}
 
-// You can explore the built-in icon families and icons on the web at:
+// // You can explore the built-in icon families and icons on the web at:
 // https://icons.expo.fyi/
-// function TabBarIcon(props: { name: string; color: string }) {
-//   return <Ionicons size={30} style={{ marginBottom: -3 }} {...props} />;
-// }
+function TabBarIcon(props: { name: string; color: string }) {
+  return <Ionicons size={30} style={{ marginBottom: -3 }} {...props} />;
+}
 
 // Each tab has its own navigation stack, you can read more about this pattern here:
 // https://reactnavigation.org/docs/tab-based-navigation#a-stack-navigator-for-each-tab
@@ -74,7 +74,7 @@ const Header = () => (
 export const TabOneNavigator = () => {
   return (
     <TabOneStack.Navigator
-      screenOptions={{
+      screenOptions={{  
         headerStyle: {
           height: 96,
         },
@@ -89,16 +89,16 @@ export const TabOneNavigator = () => {
   );
 };
 
-// const TabTwoStack = createStackNavigator<TabTwoParamList>();
+const TabTwoStack = createStackNavigator<TabTwoParamList>();
 
-// function TabTwoNavigator() {
-//   return (
-//     <TabTwoStack.Navigator>
-//       <TabTwoStack.Screen
-//         name="TabTwoScreen"
-//         component={TabTwoScreen}
-//         options={{ headerTitle: "Tab Two Title" }}
-//       />
-//     </TabTwoStack.Navigator>
-//   );
-// }
+function TabTwoNavigator() {
+  return (
+    <TabTwoStack.Navigator>
+      <TabTwoStack.Screen
+        name="TabTwoScreen"
+        component={TabTwoScreen}
+        options={{ headerTitle: "Tab Two Title" }}
+      />
+    </TabTwoStack.Navigator>
+  );
+}
